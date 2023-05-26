cdc2e612960d9ee0c269a6fc511ff85f2a5b9865
arozynek <panna.anna.r@gmail.com>
2023-05-26 11:09:25 +0200

    Fix emoji models

--- src/app/emojis/model/rendered-emojis.ts
+++ src/app/emojis/model/rendered-emojis.ts
@@ -1,12 +1,11 @@
-
-import { Emoji } from "./emoji";
+import {Emoji} from './emoji';
 
 export interface RenderedEmoji {
-    id: number;
-    character: string;
-    group: string;
+  id: number;
+  character: Emoji['character'];
+  group: Emoji['group'];
 }
 
 export interface RenderedEmoji extends Array<RenderedEmoji> {
-    length: number;
+  length: number;
 }
